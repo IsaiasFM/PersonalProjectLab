aae4c1531c4e2534cdf5508f950d6115f2b171af
IsaiasFM <59981709+IsaiasFM@users.noreply.github.com>
2020-03-17 11:32:25 -0500

    Update Program.cs

--- PersonalProjectLab/PersonalProjectLab/Program.cs
+++ PersonalProjectLab/PersonalProjectLab/Program.cs
@@ -6,7 +6,54 @@ namespace PersonalProjectLab
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int continueProcess = 0;
+            List<string> groceryList = new List<string>();
+
+            while (continueProcess != 0)
+            {
+                continueProcess = PrintOptions();
+
+                if (continueProcess == 1)
+                {
+                    groceryList.Add(Console.ReadLine());
+                }
+
+                else if (continueProcess == 2)
+                {
+
+                }
+
+                else if (continueProcess == 3)
+                {
+
+                }
+
+                else if (continueProcess == 4)
+                {
+
+                }
+
+            }
+            Console.WriteLine("Thank you! Have a nice day!");
+        }
+
+        public static int PrintOptions()
+        {
+            int chosenOption;
+            Console.WriteLine("What would you like to do?");
+            Console.WriteLine("1 - Add item");
+            Console.WriteLine("2 - Edit item");
+            Console.WriteLine("3 - Delete item");
+            Console.WriteLine("4 - Print List");
+            Console.WriteLine("0 - Exit");
+
+            chosenOption = int.Parse(Console.ReadLine());
+            return chosenOption;
+        }
+
+        public static void Printlist()
+        {
+
         }
     }
 }
